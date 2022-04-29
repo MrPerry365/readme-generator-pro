6201afe3ce55a6c800dedb86e5c020b77ab49993
MrPerry365 <1987nowhere@gmail.com>
2022-04-28 21:27:50 -0400

    add inquire and initial question

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message:"What is your project title?",
+
+    },
+    {
+        type: "input",
+        name: "author",
+        message:"What is the authors name?",
+        
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+const template = generateMarkdown(data)
+console.log(template);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions) 
+
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      console.log(answers);
+      writeToFile("test.md", answers);
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
