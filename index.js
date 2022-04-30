c86167be286c291c23b2090483ba90b12b0b6ef6
MrPerry365 <1987nowhere@gmail.com>
2022-04-30 11:28:19 -0400

    add questions

--- index.js
+++ index.js
@@ -5,18 +5,84 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
+
+// title of project //
     {
         type: "input",
         name: "title",
         message:"What is your project title?",
 
     },
+// Github username //
+    {
+        type: "input",
+        name: "username",
+        message:"What is your GitHub username?",
+
+    },
+// link to GitHub profile //
+    {
+        type: "input",
+        name: "profile",
+        message:"What is the link to your GitHub profile?",
+
+    },
+// Email address //
+    {
+        type: "input",
+        name: "email",
+        message:"What is your email address?",
+
+    },
+// Badges //    
+    {
+        type: "input",
+        name: "badge",
+        message:"What badge do you choose?",
+
+    },
+// Installation //
+    {
+        type: "type",
+        name: "installation",
+        message:"What installation is necessary for your project?",
+
+    },
+// Usage //
+    {
+        type: "input",
+        name: "usage",
+        message:"Describe the usage of your project?",
+
+    },
+// License //
+    {
+        type: "input",
+        name: "license",
+        message:"Choose the license for your project.",
+
+    },
+// Contributing //
+    {
+        type: "input",
+        name: "contributors",
+        message:"Are there other contributors?",
+
+    },
+// Tests //
+    {
+        type: "input",
+        name: "test",
+        message:"What are your project tests?",
+
+    },
+
     {
         type: "input",
         name: "author",
         message:"What is the authors name?",
         
-    }
+    },
 
 ];
 
